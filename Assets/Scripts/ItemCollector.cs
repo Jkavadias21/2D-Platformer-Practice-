076e1de660b9a175bee47ee41467d8cfb4d1722d
Jkavadias21 <johnnykavadias@gmail.com>
2023-01-22 21:50:45 +1100

    implemented multiple levels, end and start screens

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -9,13 +9,15 @@ public class ItemCollector : MonoBehaviour
     private int apples = 0;
     [SerializeField] private TextMeshProUGUI applesText;
 
+    [SerializeField] private AudioSource collectionSoundEffect;
+
     private void OnTriggerEnter2D(Collider2D collision) {
         
         if (collision.CompareTag("Apple"))
         {
+            collectionSoundEffect.Play();
             Destroy(collision.gameObject);
             apples++;
-
             applesText.text = "Apples:" + apples;
             
         }
